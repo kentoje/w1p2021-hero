f1e383fd5b22a0e6b66cd690725591fb08bc2ef4
TristanLemire <tristan.lemire.sio@gmail.com>
2019-05-06 10:46:18 +0200

    add data.json

--- app/components/Game.vue
+++ app/components/Game.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="big-header">
-    <h1>Game</h1>
+    <h1>{{ message }}</h1>
     <br />
     <router-link class="button" to="/game/2">Go to phase 2</router-link>
     <router-link class="button" to="/game/3">Go to phase 3</router-link>
@@ -10,7 +10,25 @@
 </template>
 
 <script>
+import game from "../data/data.json";
+
 export default {
-    
+    computed: {
+      id() {
+        return this.$route.params.id;
+      },
+      message() {
+        return game[this.id].message;
+      },
+      actionA() {
+        return game[this.id].actionA;
+      },
+      actionB() {
+        return game[this.id].actionB;
+      },
+      fin() {
+        return game[this.id].fin;
+      }
+    }
 };
 </script>
