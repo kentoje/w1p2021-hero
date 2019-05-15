b98d0be4a1a4d97858e2111be65e90288637e416
kentoje <k.monthubert@gmail.com>
2019-05-15 09:11:37 +0200

    add transition

--- app/components/Game.vue
+++ app/components/Game.vue
@@ -1,13 +1,36 @@
-<template>
+<!--<template>
   <div class="big-header">
     <h1>{{ message }}</h1>
     <br />
-    <router-link class="button" :to="actionA"> {{ messageA }} </router-link>
-    <router-link class="button" :to="actionB" v-if="actionB">{{ messageB }}</router-link>
-    <router-link class="button" :to="actionC" v-if="actionC">{{ messageC }}</router-link>
-    <router-link class="button" :to="actionD" v-if="actionD">{{ messageD }}</router-link>
-    <router-link class="button" :to="actionE" v-if="actionE">{{ messageE }}</router-link>
-    <router-link class="button" :to="actionF" v-if="actionF">{{ messageF }}</router-link>
+    <transition name="fadegame">
+      <div class="button__container" @click="handleFade" v-if="fade">
+        <router-link class="button" :to="actionA"> {{ messageA }} </router-link>
+        <router-link class="button" :to="actionB" v-if="actionB">{{ messageB }}</router-link>
+        <router-link class="button" :to="actionC" v-if="actionC">{{ messageC }}</router-link>
+        <router-link class="button" :to="actionD" v-if="actionD">{{ messageD }}</router-link>
+        <router-link class="button" :to="actionE" v-if="actionE">{{ messageE }}</router-link>
+        <router-link class="button" :to="actionF" v-if="actionF">{{ messageF }}</router-link>
+      </div>
+    </transition>
+  </div>
+</template> -->
+
+<template>
+  <div class="game__content">
+    <transition name="fadegame">
+      <div class="big-header" v-if="fade">
+        <h1>{{ message }}</h1>
+        <br />
+        <div class="button__container" @click="handleFade">
+          <button class="button" :to="actionA"> {{ messageA }} </button>
+          <button class="button" :to="actionB" v-if="actionB">{{ messageB }}</button>
+          <button class="button" :to="actionC" v-if="actionC">{{ messageC }}</button>
+          <button class="button" :to="actionD" v-if="actionD">{{ messageD }}</button>
+          <button class="button" :to="actionE" v-if="actionE">{{ messageE }}</button>
+          <button class="button" :to="actionF" v-if="actionF">{{ messageF }}</button>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -15,6 +38,12 @@
 import game from "../data/data.json";
 
 export default {
+    data() {
+      return { 
+        fade: true
+      }
+    },
+    
     computed: {
       id() {
         return this.$route.params.id;
@@ -60,6 +89,15 @@ export default {
       },
       fin() {
         return game[this.id].fin;
+      }
+    },
+    methods: {
+      handleFade() {
+        this.fade = !this.fade;
+
+        setTimeout(() => {
+          this.fade = !this.fade;
+        }, 1000);
       }
     }
 };
