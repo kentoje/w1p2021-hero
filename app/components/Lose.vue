716e9b9f00d1e714a2b34086b8c391a2fbf040ce
kentoje <k.monthubert@gmail.com>
2019-05-20 11:16:54 +0200

    change css and data json

--- app/components/Lose.vue
+++ app/components/Lose.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="big-header">
-    <h1>{{ message }} en {{ nbEtape }} etapes</h1>
+    <h1>{{ message }} en {{ nbEtape }} étapes.</h1>
     <br>
     <button class="button" @click="goToHome">Go to Home</button>
   </div>
@@ -24,7 +24,7 @@ export default {
   },
   data() {
     return {
-      message: "Perdu"
+      message: "Vous avez perdu"
     };
   }
 };
